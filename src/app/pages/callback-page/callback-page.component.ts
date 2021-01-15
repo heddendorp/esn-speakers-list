d3a06b2b7b97d4146e249b619482b827f8690460
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2021-01-15 17:24:42 +0100

    Update dependencies

--- src/app/pages/callback-page/callback-page.component.ts
+++ src/app/pages/callback-page/callback-page.component.ts
@@ -1,28 +1,28 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-callback-page',
-  template: `
-    <p>
-      Logging in, please wait.
-    </p>
-  `,
+  template: ` <p>Logging in, please wait.</p> `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CallbackPageComponent {
-  constructor(route: ActivatedRoute, auth: AngularFireAuth, router: Router) {
+  constructor(
+    route: ActivatedRoute,
+    fireAuth: AngularFireAuth,
+    router: Router
+  ) {
     const token = route.snapshot.queryParamMap.get('token');
     if (!token) {
       router.navigate(['/start']);
     }
-    auth
+    fireAuth
       .setPersistence(firebase.auth.Auth.Persistence.SESSION)
       .then(() =>
-        auth
+        fireAuth
           .signInWithCustomToken(token)
           .then(() => router.navigate(['/lists']))
       );
